399a6610f7e5f4e61a517d45e13aaad12667da5e
Koltsun Nikita <koltsun-nv@yandex.ru>
2023-01-20 01:52:06 +0300

    video Player

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+import { radioPlayerInit } from "./radioPlayer.js";
+import { videoPlayerInit } from "./videoPlayer.js";
+import { musicPlayerInit } from "./musicPlayer.js";
+
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
+
+const deactivationPlayer = () =>{
+    temp.style.display = 'none';
+    playerBtn.forEach(item => item.classList.remove('active'));
+    playerBlock.forEach(item => item.classList.remove('active'));
+};
+
+
+playerBtn.forEach((btn,i)=>{
+    btn.addEventListener('click', ()=>{
+        deactivationPlayer();
+        btn.classList.add('active');
+        playerBlock[i].classList.add('active');
+    });
+});
+
+
+radioPlayerInit();
+videoPlayerInit();
+musicPlayerInit();
---
+++ scripts/videoPlayer.js
@@ -0,0 +1,72 @@
+export const videoPlayerInit = () =>{
+
+    const videoPlayer = document.querySelector('.video-player');
+    const videoButtonPlay = document.querySelector('.video-button__play');
+    const videoButtonStop = document.querySelector('.video-button__stop');
+    const videoProgress = document.querySelector('.video-progress');
+    const videoTimePassed = document.querySelector('.video-time__passed');
+    const videoTimeTotal = document.querySelector('.video-time__total');
+
+    const toggleIcon = () =>{
+        if (videoPlayer.paused){
+            videoButtonPlay.classList.remove('fa-pause');
+            VideoColorSpace.classList.add('fa-play');
+        }
+        else{
+            videoButtonPlay.classList.add('fa-pause');
+            VideoColorSpace.classList.remove('fa-play');
+        }
+
+    }
+
+    const togglePlay = ()=>{
+        if (videoPlayer.paused){
+            videoPlayer.play();
+        }
+        else{
+            videoPlayer.pause();
+        }
+        
+        toggleIcon();
+    };
+
+    const stopPlay = () =>{
+        videoPlayer.pause();
+        videoPlayer.currentTime = 0;
+    };
+
+    const addZero = n => n < 10 ? '0' + n : n;
+
+    videoPlayer.addEventListener('click', togglePlay);
+    videoButtonPlay.addEventListener('click', togglePlay);
+
+    videoPlayer.addEventListener('play', toggleIcon);
+    videoPlayer.addEventListener('pause', toggleIcon);
+
+    videoButtonStop.addEventListener('click', stopPlay);
+
+
+    videoPlayer.addEventListener('timeupdate', ()=>{
+        const currentTime = videoPlayer.currentTime;
+        const duration = videoPlayer.duration;
+
+        videoProgress.value = (currentTime / duration) * 100;
+
+        let minutePassed = Math.floor(currentTime / 60);
+        let secondsPassed = Math.floor(currentTime % 60);
+
+        let minuteTotal = Math.floor(duration / 60);
+        let secondsTotal = Math.floor(duration % 60);
+
+        videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`;
+        videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`;
+
+    });
+
+    videoProgress.addEventListener('change', () =>{
+        const duration = videoPlayer.duration;
+        const value = videoProgress.value;
+
+        videoPlayer.currentTime = (value * duration) / 100;
+    });
+};
